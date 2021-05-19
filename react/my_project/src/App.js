361c72d404714a53e46862466cc2f1e7192ab2cd
AlexeiLazar87 <AlexeiLazar87@gmail.com>
2021-05-19 16:42:32 +0300

    react_hw7

--- react/my_project/src/App.js
+++ react/my_project/src/App.js
@@ -10,7 +10,7 @@ const Todo = () => {
     return (
         <div>
             <div>
-                <h1>Todo List: {todo}</h1>
+                <h1>Todo: {todo}</h1>
                 <input type="text" name={'input'} value={value} placeholder={'Add something...'}
                        onChange={({target: {value}}) => setValue(value)}/>
                 <div>
