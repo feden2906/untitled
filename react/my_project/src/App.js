1d46819884c6f1cbedb7e156ac23340f770c676f
AlexeiLazar87 <AlexeiLazar87@gmail.com>
2021-05-19 19:12:56 +0300

    react_practice7

--- react/my_project/src/App.js
+++ react/my_project/src/App.js
@@ -1,25 +1,10 @@
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
-import Component from "./components/Counter";
+
+
 
 export default function App() {
     return (
         <div>
-            <Router>
-                <Link to={'./components/Counter'}>to Counter</Link>
-
-                <Switch>
-
-                    <Route exact path={'./components/Counter'} render={() => {
-                        <Component/>
-                    }}/>
-                </Switch>
-
-            </Router>
+           App
         </div>
     );
 }
