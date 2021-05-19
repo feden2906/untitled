0e3f4f5a842f3b0b763db45f5106a54132c059e5
AlexeiLazar87 <AlexeiLazar87@gmail.com>
2021-05-19 16:17:36 +0300

    react_hw7

--- react/my_project/src/App.js
+++ react/my_project/src/App.js
@@ -1,8 +1,40 @@
+import {useSelector, useDispatch} from "react-redux";
+import {useState} from "react";
+import  from "./App.css"
+
+
+const Todo = () => {
+    const todo = useSelector((state) => state.todo)
+    const dispatch = useDispatch();
+    const [value, setValue] = useState('')
+    const [check, setCheck] = useState(null)
+    return (
+        <div className={wrap}>
+            <h1>Todo List: {todo}</h1>
+            <input type="text" name={'input'} value={value} placeholder={'Add something...'} onChange={({target: {value}}) => setValue(value)}/>
+            <div>
+                <h4>Market completed?</h4>
+                <hr/>
+                YES <input type="radio" name={'checkStatus'} value={true}
+                       onChange={({target: {value}}) => setCheck(value)}/>
+                <hr/>
+                NO <input type="radio" name={'checkStatus'} value={false}
+                       onChange={({target: {value}}) => setCheck(value)}/>
+                <hr/>
+            </div>
+            <div>
+                <button onClick={() => {
+                    dispatch({type: "ADD_TO_LIST", payload: {input: value, checkStatus: check}})
+                }}>ADD</button>
+            </div>
+        </div>
+    )
+}
 
 export default function App() {
     return (
         <div>
-            App
+            <Todo/>
         </div>
     );
 }
--- react/my_project/src/index.js
+++ react/my_project/src/index.js
@@ -3,12 +3,48 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {createStore} from "redux";
+import {Provider} from "react-redux";
+
+const initialState = {
+    Todo: []
+}
+
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case "ADD_TO_LIST": {
+            return {
+                ...state,
+                Todo: state.Todo.push(action.payload)
+            }
+        }
+        case "MARKET_COMPLETED": {
+            return {
+                ...state,
+                Todo: state.Todo /// ???
+            }
+        }
+        case "REMOVE": {
+            return {
+                ...state,
+                Todo: state.Todo = initialState  /// ???
+            }
+        }
+        default:
+            return state
+    }
+}
+
+const store = createStore(reducer);
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <Provider store={store}>
+            <App/>
+        </Provider>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
